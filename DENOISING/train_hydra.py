9a4144b94f1d3328a9793cfea16072622c8d7007
Egor <geksut@gmail.com>
2020-08-22 20:56:23 +0300

    denois backup

---
+++ DENOISING/train_hydra.py
@@ -0,0 +1,52 @@
+import os
+import hydra
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from model.unet_basic import Model as Unet
+from dataset.waveform_dataset import DatasetAudio
+from trainer.trainer import Trainer
+from model.loss import mse_loss, l1_loss
+
+
+@hydra.main(config_path='config_h.yaml')
+def main(config):
+    torch.manual_seed(config["seed"])  # for both CPU and GPU
+    np.random.seed(config["seed"])
+
+    train_set = DatasetAudio(**config.data.dataset_train)
+    train_dataloader = DataLoader(dataset=train_set,**config.data.loader_train)
+
+    val_set = DatasetAudio(**config.data.dataset_val)
+    val_dataloader = DataLoader(
+        dataset=val_set,
+        num_workers=1,
+        batch_size=1,
+        shuffle=False)
+
+    model = Unet(**config.model)
+
+    optimizer = torch.optim.Adam(
+        params=model.parameters(),
+        lr=config.optim.lr,
+        betas=(config.optim.beta1, config.optim.beta2)
+    )
+
+    loss_function = globals()[config.loss]
+
+    trainer_class = initialize_config(config["trainer"], pass_args=False)
+
+    trainer = trainer_class(
+        config=config.trainer,
+        model=model,
+        loss_function=loss_function,
+        optimizer=optimizer,
+        train_dataloader=train_dataloader,
+        validation_dataloader=valid_dataloader
+    )
+
+    trainer.train()
+
+if __name__ == '__main__':
+    main()
---
+++ hydra/hydra_test.py
@@ -0,0 +1,21 @@
+import hydra
+
+
+def some_print(x, driver, pw, user):
+    print(x)
+    print(driver)
+    print(pw)
+    print(user)
+
+
+@hydra.main(config_path='config.yaml')
+def my_app(cfg):
+    print(cfg.db.pretty())
+    driver = cfg.db.driver 
+    user = cfg.db.user 
+    
+    some_print(x='h', **cfg.db)
+
+
+if __name__ == "__main__":
+    my_app()
