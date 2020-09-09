2d9db653ffa378fc1387eb24e45007817f18afe8
Egor <geksut@gmail.com>
2020-09-09 23:47:04 +0300

    a

--- hydra/hydra_test.py
+++ hydra/hydra_test.py
@@ -8,14 +8,17 @@ def some_print(x, driver, pw, user):
     print(user)
 
 
-@hydra.main(config_path='config.yaml')
+@hydra.main(config_path="config.yaml")
 def my_app(cfg):
     print(cfg.db.pretty())
-    driver = cfg.db.driver 
-    user = cfg.db.user 
-    
-    some_print(x='h', **cfg.db)
+    driver = cfg.db.driver
+    user = cfg.db.user
+
+    some_print(x="h", **cfg.db)
 
 
 if __name__ == "__main__":
     my_app()
+
+
+
